961bf8df83fad779d926fbb226114bccf1d0d022
Sai Ram <117701637+sairam030@users.noreply.github.com>
2024-03-30 13:39:54 +0530

    first commit

---
+++ lib/src/common/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color.fromRGBO(63, 92, 184, 1);
+  static const Color accentColor = Color.fromRGBO(254, 176, 82, 1);
+  static const Color textColor = Colors.black;
+  static const Color secondaryTextColor = Color.fromRGBO(107, 114, 128, 1);
+  static const Color dividerColor = Color.fromRGBO(209, 213, 219, 1);
+  static const Color scaffoldBackgroundColor = Color.fromRGBO(241, 245, 246, 1);
+  static const Color whiteColor = Colors.white;
+  static const Color searchBarColor = Color.fromRGBO(233, 233, 233, 1);
+  static const Color blue = Colors.blue;
+
+// Add any additional colors you may use in your application
+}
